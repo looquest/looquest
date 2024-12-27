a0a96225d3389efd0a437c5a309d8095843a8d18
gt <59036640+GauravTalreja@users.noreply.github.com>
2024-12-27 09:39:42 -0800

    base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/browser-vector-search/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@electric-sql/pglite'],
